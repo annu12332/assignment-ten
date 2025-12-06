6c475c8e337dbb58be379cdd65790ad2ba269227
md anas <anas2542wy@gmail.com>
2025-12-07 01:51:51 +0600

    added my ads and my ads ui

--- src/pages/MyAds.jsx
+++ src/pages/MyAds.jsx
@@ -19,6 +19,57 @@ const MyAds = () => {
     return (
         <div>
             My Ads {myAds.length}
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        {myAds.map((ads, index) =>
+                            <tr key={index}>
+                                <th>{index + 1}</th>
+
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img src={ads?.image} alt="service" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{ads?.name}</div>
+                                            
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td>
+                                    {ads?.description}
+                                    <br />
+                                </td>
+
+                                <td>{ads?.price }</td>
+
+                                <th className='flex gap-2 '>
+                                    <button className="btn font-bold btn-info btn-xs">Edit</button>
+                                    <button className="btn btn-error font-bold btn-xs">Delete</button>
+                                </th>
+
+                            </tr>
+                        )}
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
